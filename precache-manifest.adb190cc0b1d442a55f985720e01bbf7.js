3b6a2714b7369ee29d5270494efb83d67b470384
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-05 12:47:53 -0800

    Updates

---
+++ precache-manifest.adb190cc0b1d442a55f985720e01bbf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6adf6ab94dc3d9dc5793f3d7e21d09",
+    "url": "/FEW1.2-assessment/index.html"
+  },
+  {
+    "revision": "3209053aa85c70d6abfb",
+    "url": "/FEW1.2-assessment/static/css/main.832e7bbb.chunk.css"
+  },
+  {
+    "revision": "14dbc222178af5e2305d",
+    "url": "/FEW1.2-assessment/static/js/2.b89f45a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FEW1.2-assessment/static/js/2.b89f45a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3209053aa85c70d6abfb",
+    "url": "/FEW1.2-assessment/static/js/main.597c9273.chunk.js"
+  },
+  {
+    "revision": "5ed437d9a059fe2a68d1",
+    "url": "/FEW1.2-assessment/static/js/runtime-main.0ffa000a.js"
+  }
+]);
